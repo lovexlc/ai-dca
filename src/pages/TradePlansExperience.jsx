15abcf14f52d51c3160dd75f90141f7f1832efdb
lovexl <lovexl@local>
2026-03-30 08:48:10 +0000

    Refresh notify setup after admin token

--- src/pages/TradePlansExperience.jsx
+++ src/pages/TradePlansExperience.jsx
@@ -64,7 +64,7 @@ export function TradePlansExperience({ links, embedded = false }) {
     return () => {
       cancelled = true;
     };
-  }, []);
+  }, [adminToken]);
 
   useEffect(() => {
     if (!previewRows.length) {
