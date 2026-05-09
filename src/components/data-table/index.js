fc194b02e24679e72c181f74925a814da0c75c85
lumstu <101900306@hbut.edu.cn>
2026-05-09 12:26:50 +0800

    Revert "feat(holdings): 接入 tablecn风格的 DataTable（TanStack Table 驱动）"
    This reverts commit 103bbe1535fca7b7615655db65624acc17efaa3c.

--- src/components/data-table/index.js
+++
@@ -1,7 +0,0 @@
-export { Popover } from './Popover.jsx';
-export { DataTable } from './DataTable.jsx';
-export { DataTableColumnHeader } from './DataTableColumnHeader.jsx';
-export { DataTableFacetedFilter } from './DataTableFacetedFilter.jsx';
-export { DataTablePagination } from './DataTablePagination.jsx';
-export { DataTableToolbar } from './DataTableToolbar.jsx';
-export { DataTableViewOptions } from './DataTableViewOptions.jsx';
